826e89cb04af6b03a349e5e02cc751cb291593d6
Pau <pau.anand.morello@gmail.com>
2025-06-30 21:25:00 +0200

    libreria general

---
+++ includes/general.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   general.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmorello <pmorello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 21:22:10 by pmorello          #+#    #+#             */
+/*   Updated: 2025/06/30 21:23:43 by pmorello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GENERAL_H
+#define GENERAL_H
+
+#include "../mlx/mlx.h"
+#include "../libft/libft.h"
+#include "window.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#endif
